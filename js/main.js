e7fa15bfac3a2c7b97f3e07606657f894817df5f
Kareem Nasman <79852385+KareemNasman@users.noreply.github.com>
2021-10-02 00:43:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// var s = skrollr.init();
+AOS.init();
+   $(function () {
+
+    $('.lds-hourglass').delay(1000).fadeOut('slow', function () {
+        $('body').css("overflow","auto");
+      });
+     }) 
